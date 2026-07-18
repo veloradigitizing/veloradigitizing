72085f41e0b075e6493a00a7a7217ddfca925a60
vesper digitizing <vesperdigitizing@gmail.com>
2026-07-19 01:01:17 +0500

    chore: remove deprecated BundleCarousel and StoreBundles components

--- app/components/BundleCarousel.tsx
+++
@@ -1,2 +0,0 @@
-// DEPRECATED - Use BundleSection from ../store/BundleSection instead
-// This file is no longer used
--- app/store/StoreBundles.tsx
+++
@@ -1,2 +0,0 @@
-// DEPRECATED - Use BundleSection from ./BundleSection instead
-// This file is no longer used
